adc988a863c2b38fea3fe603a1b54c84f3cfb2ee
Ladislav Hovan <19410951+ladislav-hovan@users.noreply.github.com>
2023-03-24 11:13:11 +0100

    Remove uniprot_api.py, implement a replacement

--- sponge/functions.py
+++ sponge/functions.py
@@ -1,21 +1,211 @@
 ### Imports ###
 import pandas as pd
 import bioframe
+import requests
+import os
+import time
+
+from typing import Optional, Union, Iterable, Mapping
+
+from biomart import BiomartServer
+
+from io import BytesIO
+
+from collections import defaultdict
 
 from math import log2
 
+from tqdm import tqdm
+
+from io import BytesIO
+
 ### Data ###
-# TODO: Add URLs
-file_df = pd.DataFrame(
-    {'description': ['homologene', 'promoter', 'jaspar_bigbed'],
-     'name': ['homologene.tsv', 'promoters.bed', 'JASPAR.bb'],
+FILE_DF = pd.DataFrame(
+    {'description': ['homologene', 'promoter', 'jaspar_bigbed', 'ensemble'],
+     'name': ['homologene.tsv', 'promoters.bed', 'JASPAR.bb', 'ensemble.tsv'],
      'url': ['https://ftp.ncbi.nih.gov/pub/HomoloGene/current/homologene.data',
-             '',
-             'http://expdata.cmmt.ubc.ca/JASPAR/downloads/UCSC_tracks/{year}/JASPAR{year}_hg38.bb'],
-     'mode': ['w', 'w', 'wb']}
+             None,
+             'http://expdata.cmmt.ubc.ca/JASPAR/downloads/UCSC_tracks/{year}'
+             '/JASPAR{year}_hg38.bb',
+             None],
+     'eval': [None,
+              'self.load_promoters_from_biomart(**options)',
+              None,
+              'self.load_ensemble_from_biomart(**options)']}
 ).set_index('description')
 
+ENSEMBL_URL = 'http://www.ensembl.org/biomart'
+
 ### Functions ###
+def download_with_progress(
+    url: Union[str, requests.models.Response],
+    file_path: Optional[str] = None,
+    desc: str = 'response'
+):
+    
+    if type(url) == str:
+        request = requests.get(url, stream=True)
+    else:
+        request = url
+    total = int(request.headers.get('content-length', 0))          
+    if file_path is None:
+        stream = BytesIO()
+    else:
+        stream = open(file_path, 'wb')
+        desc = file_path
+    with tqdm(desc=desc, total=total, unit='iB', unit_scale=True,
+        unit_divisor=1024) as bar:
+        for data in request.iter_content(chunk_size=1024):
+            size = stream.write(data)
+            bar.update(size)
+    
+    if file_path is None:
+        return BytesIO(stream.getvalue())
+
+
+def get_uniprot_mapping(
+    from_db: str,
+    to_db: str,
+    ids: Union[str, Iterable[str]],
+    **kwargs
+):
+    
+    MAPPING_URL = 'https://rest.uniprot.org/idmapping/'
+    data = {'ids': ids, 'from': from_db, 'to': to_db}
+    data.update(kwargs)
+    uniprot_request = requests.post(MAPPING_URL + 'run', data)
+    uniprot_reply = uniprot_request.json()
+    if 'jobId' in uniprot_reply:
+        job_id = uniprot_reply['jobId']
+    else:
+        print ('Unable to retrieve a job ID from UniProt')
+        print ('The following reply was received instead:')
+        for message in uniprot_reply['messages']:
+            print (message)
+        raise RuntimeError()
+    while True:
+        uniprot_status = requests.get(MAPPING_URL + f'status/{job_id}')
+        if 'results' in uniprot_status.json():
+            break
+        time.sleep(0.5)
+    uniprot_results = requests.get(MAPPING_URL + f'stream/{job_id}')
+    results_df = pd.DataFrame(uniprot_results.json()['results'])
+    results_df.drop_duplicates(subset='from', inplace=True)
+    return results_df
+
+
+def process_ensemble_df(
+    df
+):
+
+    pass
+
+
+def load_promoters_from_biomart(
+    file_path,
+    filter_basic: bool = True,
+    chromosomes: Iterable[str] = [str(i) for i in range(1,23)] + 
+        ['MT', 'X', 'Y'],
+    save_ensemble: bool = True
+):
+
+    bm_server = BiomartServer(ENSEMBL_URL)
+    ensembl = bm_server.datasets['hsapiens_gene_ensembl']
+    attributes = ['ensembl_transcript_id', 'transcript_gencode_basic', 
+        'chromosome_name', 'transcription_start_site', 'strand']
+    if save_ensemble:
+        attributes += ['ensembl_gene_id', 'external_gene_name', 'gene_biotype']
+    print ('Retrieving response to query...')
+    dtype_dict = defaultdict(lambda: str)
+    dtype_dict['Transcription start site (TSS)'] = int
+    dtype_dict['Strand'] = int
+    response = ensembl.search({'attributes': attributes}, header=1)
+    buffer = download_with_progress(response)
+    df = pd.read_csv(buffer, sep='\t', dtype=dtype_dict)
+    print ('Filtering and modifying dataframe...')
+    if filter_basic:
+        df = df[df['GENCODE basic annotation'] == 'GENCODE basic'].copy()
+        df.drop(columns='GENCODE basic annotation', inplace=True)
+    if chromosomes is not None:
+        df = df[df['Chromosome/scaffold name'].isin(chromosomes)]
+    df['Chromosome'] = df['Chromosome/scaffold name'].apply(lambda x: 'chrM' 
+        if x == 'MT' else f'chr{x}')
+    df['Strand'] = df['Strand'].apply(lambda x: '+' if x > 0 else '-')
+    df['Start'] = df.apply(lambda row: 
+        row['Transcription start site (TSS)'] - 750 if row['Strand'] == '+' 
+        else row['Transcription start site (TSS)'] - 250, axis=1)
+    df['End'] = df['Start'] + 1000
+    df['Score'] = 0
+    df.sort_values(['Chromosome', 'Start'], inplace=True)
+    columns = ['Chromosome', 'Start', 'End', 'Transcript stable ID', 'Score', 
+        'Strand']
+    print (f'Saving data to {file_path}...')
+    df[columns].to_csv(file_path, sep='\t', header=False, index=False)
+    print ()
+    if save_ensemble:
+        columns = ['Gene stable ID', 'Transcript stable ID', 'Gene name', 
+            'Gene type']
+        process_ensemble_df(df[columns])
+
+
+def load_ensemble_from_biomart(
+    file_path
+):
+    
+    pass
+
+
+def retrieve_file(
+    description,
+    temp_folder,
+    prompt = True,
+    jaspar_release = None
+):
+
+    
+    if description not in FILE_DF.index:
+        print (f'File description not recognised: {description}')
+        return
+    if not os.path.exists(temp_folder):
+        os.mkdir(temp_folder)
+    file_name = FILE_DF.loc[description, 'name']
+    file_path = os.path.join(temp_folder, file_name)
+    if os.path.exists(file_path):
+        print (f'Using cached file {file_path}')
+        print ()
+    else:
+        print (f'File {file_name} not found in directory {temp_folder}')
+        if prompt:
+            key = None
+            positive = ['y', 'yes', 'hell yeah']
+            negative = ['n', 'no', 'nope']
+            while key is None or key.lower() not in positive + negative:
+                if key is not None:
+                    print (f'Input not recognised: {key}')
+                print ('Do you want to download it? Y/N', flush=True)
+                key = input()
+                print (key)
+            print ()
+            if key.lower() in negative:
+                return None
+        to_request = FILE_DF.loc[description, 'url']
+        if to_request is None:
+            # These options are not unused: they are passed to the
+            # evaluated function call as kwargs
+            options = {'file_path': file_path}
+            eval(FILE_DF.loc[description, 'eval'])
+        else:
+            if description == 'jaspar_bigbed':
+                if jaspar_release is None:
+                    raise ValueError('The release of jaspar has to be '
+                        'specified in order to retrieve the bigbed file')
+                to_request = to_request.format(year=jaspar_release[-4:])
+            print (f'Downloading data into {file_path}...')
+            download_with_progress(to_request, file_path)    
+
+    return file_path
+
+
 def filter_edges(
     bb_ref, 
     bed_df, 
@@ -30,7 +220,8 @@ def filter_edges(
     for transcript in bed_df.index[start_ind:final_ind]:
         start,end = bed_df.loc[transcript][['start', 'end']]
         motifs = bioframe.read_bigbed(bb_ref, chrom, start=start, end=end)
-        max_scores = motifs[motifs['TFName'].isin(motif_list)].groupby('TFName')[['score', 'name']].max()
+        max_scores = motifs[motifs['TFName'].isin(motif_list)].groupby(
+            'TFName')[['score', 'name']].max()
         max_scores = max_scores[max_scores['score'] >= score_threshold]
         max_scores.reset_index(inplace=True)
         max_scores['transcript'] = transcript
@@ -62,11 +253,13 @@ def calculate_ic(
     motif
 ):
     """
-    Calculates the information content for a given motif, assumes equal ACGT distribution.
+    Calculates the information content for a given motif, assumes equal 
+    ACGT distribution.
     """
 
     df = pd.DataFrame(motif.pwm)
-    df['IC'] = df.apply(lambda x: 2 + sum([plogp(x[y]) for y in ['A', 'C', 'G', 'T']]), axis=1)
+    df['IC'] = df.apply(lambda x: 2 + sum([plogp(x[y]) for y in 
+        ['A', 'C', 'G', 'T']]), axis=1)
     
     return df['IC'].sum()
 
--- sponge/sponge.py
+++ sponge/sponge.py
@@ -3,8 +3,6 @@ import pandas as pd
 import numpy as np
 import bioframe
 import time
-import os
-import requests
 
 from pyjaspar import jaspardb
 
@@ -12,7 +10,7 @@ from multiprocessing import Pool
 
 from math import *
 
-from sponge.uniprot_api import *
+# from sponge.uniprot_api import *
 from sponge.functions import *
 
 ### Class definition ###
@@ -21,12 +19,30 @@ class Sponge:
         self,
         temp_folder = '.sponge_temp/'
     ):
+        """
+        Usual order of operations:
+        sponge_obj = Sponge()
+        sponge_obj.select_tfs()
+        sponge_obj.find_human_homologs()
+        sponge_obj.filter_matches()
+        sponge_obj.retrieve_ppi()
+        sponge_obj.write_ppi_prior()
+        sponge_obj.aggregate_matches()    
+        sponge_obj.write_motif_prior()
+
+        Parameters
+        ----------
+        temp_folder : str, optional
+            _description_, by default '.sponge_temp/'
+        """
+
 
         self.temp_folder = temp_folder
 
 
     def select_tfs(
-        self
+        self,
+        drop_heterodimers = True
     ):
 
         # Database object
@@ -73,69 +89,26 @@ class Sponge:
         print ('Unique motifs:', len(motifs_unique))
 
         # Drop heterodimers
-        motifs_nohd = [i for i in motifs_unique if ':' not in i.name]
-        print ('Motifs without heterodimers:', len(motifs_nohd))
-
-        self.motifs = motifs_nohd
-
-
-    def retrieve_file(
-        self,
-        description,
-        temp_folder
-    ):
-            
-        # TODO: Promoter file needs modifications
-        
-        if description not in file_df.index:
-            print (f'File description not recognised: {description}')
-            return
-        if not os.path.exists(temp_folder):
-            os.mkdir(temp_folder)
-        file_name = file_df.loc[description, 'name']
-        file_path = os.path.join(temp_folder, file_name)
-        if os.path.exists(file_path):
-            print (f'Using cached file {file_path}')
-            print ()
+        if drop_heterodimers:
+            motifs_nohd = [i for i in motifs_unique if '::' not in i.name]
+            print ('Motifs without heterodimers:', len(motifs_nohd))
+            self.motifs = motifs_nohd
         else:
-            print (f'File {file_name} not found in directory {temp_folder}')
-            key = None
-            positive = ['y', 'yes', 'hell yeah']
-            negative = ['n', 'no', 'nope']
-            while key is None or key.lower() not in positive + negative:
-                if key is not None:
-                    print (f'Input not recognised: {key}')
-                print ('Do you want to download it? Y/N')
-                key = input()
-                print (key)
-            print ()
-            if key.lower() in negative:
-                return None
-            to_request = file_df.loc[description, 'url']
-            if description == 'jaspar_bigbed':
-                to_request.format(year=self.release[-4:])
-            r = requests.get(to_request)
-            mode = file_df.loc[description, 'mode']
-            if mode == 'w':
-                data = r.text
-            else:
-                data = r.content
-            print (f'Downloading data into {file_path}...')
-            open(file_path, mode).write(data)
-            print ()     
-
-        return file_path
+            self.motifs = motifs_unique
 
 
     def find_human_homologs(
         self, 
-        homologene_file=None
+        homologene_file = None,
+        prompt = True
     ):
         
         if homologene_file is None:
-            homologene_file = self.retrieve_file('homologene', self.temp_folder)
+            homologene_file = retrieve_file('homologene', 
+                self.temp_folder, prompt=prompt)
             if homologene_file is None:
-                print ('Unable to find or retrieve the homologene file, exiting')
+                print ('Unable to find or retrieve the homologene file, ' 
+                    'exiting')
                 return
 
         # Get the non-human motifs
@@ -164,16 +137,8 @@ class Sponge:
         # Get the missing IDs from Uniprot API
         print ()
         print ('Retrieving matches from UniProt...')
-        job_id = submit_id_mapping(
-            from_db="UniProtKB_AC-ID", to_db="RefSeq_Protein", ids=[i.acc[0] 
-            for i in non_human_motifs if i.name in missing]
-        )
-        if check_id_mapping_results_ready(job_id):
-            link = get_id_mapping_results_link(job_id)
-            results = get_id_mapping_results_search(link)
-
-        # Convert the results into a DataFrame
-        mapping = pd.DataFrame(results['results'])
+        mapping = get_uniprot_mapping("UniProtKB_AC-ID", "RefSeq_Protein",
+            [i.acc[0] for i in non_human_motifs if i.name in missing])
         mapping.columns = ['Uniprot', 'Accession']
 
         # Create a DataFrame for matching missing entries
@@ -235,36 +200,44 @@ class Sponge:
             (corr_df['Original Name'].isin(to_drop) == False)]
 
         # The mapping of original to human names and the matrix IDs to be kept
-        animal_to_human = {i: j for i,j in zip(corr_df_final['Original Name'], 
-            corr_df_final['Human Name'])}
+        animal_to_human = defaultdict(lambda x: x)
+        for animal_name, human_name in zip(corr_df_final['Original Name'], 
+            corr_df_final['Human Name']):
+            animal_to_human[animal_name] = human_name
+        # animal_to_human = {i: j for i,j in zip(corr_df_final['Original Name'], 
+        #     corr_df_final['Human Name'])}
         print ()
         print ('Final number of IDs which will be replaced by human homologs:', 
                len(animal_to_human))
-        vert_list_nohd = [i.matrix_id for i in self.motifs if 
-            (i.name in human_motif_names or i.name in animal_to_human)]
-        print ('Final number of total matrix IDs:', len(vert_list_nohd))
+        matrix_ids = [motif.matrix_id for motif in self.motifs if 
+            (motif.name in human_motif_names or motif.name in animal_to_human)]
+        print ('Final number of total matrix IDs:', len(matrix_ids))
 
-        self.matrix_ids = vert_list_nohd
+        self.matrix_ids = matrix_ids
         self.tf_names = human_motif_names + list(animal_to_human.keys())
         self.animal_to_human = animal_to_human
 
 
     def filter_matches(
         self, 
-        promoter_file=None, 
-        bigbed_file=None,
-        n_processes=1
+        promoter_file = None, 
+        bigbed_file = None,
+        n_processes = 1,
+        prompt = True
     ):
         
         if promoter_file is None:
-            promoter_file = self.retrieve_file('promoter', self.temp_folder)
+            promoter_file = retrieve_file('promoter', self.temp_folder, 
+                prompt=prompt)
             if promoter_file is None:
                 print ('Unable to find or retrieve the promoter file, exiting')
                 return
         if bigbed_file is None:
-            bigbed_file = self.retrieve_file('jaspar_bigbed', self.temp_folder)
+            bigbed_file = retrieve_file('jaspar_bigbed', self.temp_folder, 
+                prompt=prompt, jaspar_release=self.release)
             if bigbed_file is None:
-                print ('Unable to find or retrieve the JASPAR bigbed file, exiting')
+                print ('Unable to find or retrieve the JASPAR bigbed file, '
+                    'exiting')
                 return
 
         print ('Loading the promoter bed file...')
@@ -293,16 +266,49 @@ class Sponge:
             print (f'Chromosome {chrom[3:]} with ' + suffix)
             chunk_size = ceil(sqrt(len(df_chrom) / n_processes))
             chunk_divisions = [i for i in range(0, len(df_chrom), chunk_size)]
-            input_tuples = [(bigbed_file, df_chrom, self.tf_names, chrom, i, i+chunk_size)
-                for i in chunk_divisions]
-            result = p.map_async(filter_edges_helper, input_tuples, chunksize=n_processes)
+            input_tuples = [(bigbed_file, df_chrom, self.tf_names, chrom, i, 
+                i+chunk_size) for i in chunk_divisions]
+            result = p.map_async(filter_edges_helper, input_tuples, 
+                chunksize=n_processes)
             edges_chrom_list = result.get()
             results_list += edges_chrom_list
             elapsed_chr = time.time() - st_chr
-            print (f'Done in: {elapsed_chr // 60:n} m {elapsed_chr % 60:.2f} s')
+            print (f'Done in: {elapsed_chr // 60:n} m '
+                f'{elapsed_chr % 60:.2f} s')
 
         elapsed = time.time() - start_time
         print ()
         print (f'Total time: {elapsed // 60:n} m {elapsed % 60:.2f} s')
 
         self.all_edges = pd.concat(results_list, ignore_index=True)
+
+    
+    def retrieve_ppi(
+        self
+    ) -> None:
+        
+        tf_list = [self.animal_to_human[tf] for tf in 
+            self.all_edges['TFName'].unique()]
+        query_string = '%0d'.join(tf_list)
+        
+
+
+    def write_ppi_prior(
+        self
+    ) -> None:
+        
+        pass
+
+
+    def aggregate_matches(
+        self
+    ) -> None:
+        
+        pass
+
+
+    def write_motif_prior(
+        self
+    ) -> None:
+        
+        pass
